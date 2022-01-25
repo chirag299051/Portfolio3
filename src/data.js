312e058bc6050c59f391e6593732e345d4c189f1
Chirag Singh <chirag@Chirags-MacBook-Pro.local>
2022-01-25 18:37:02 +0530

    Update

--- src/data.js
+++ src/data.js
@@ -17,7 +17,7 @@ import rest from "./assets/rest.png";
 import ionic from "./assets/ionic.png";
 import fire from "./assets/fire.png";
 import google from "./assets/google.png";
-import redux from "./assets/redux.jpeg";
+import redux from "./assets/redux.png";
 import npm from "./assets/npm.png";
 
 import movieDBv2 from "./assets/movieDB-v2.png";
